8c3d51279038a8c0e8cd3967c4df888f5510b656
Benjamin <benja13bebv@gmail.com>
2021-06-09 14:28:52 -0400

    Agregar sistema de rutas

--- web/zgames/routes/web.php
+++ web/zgames/routes/web.php
@@ -13,23 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('registrar_consola');
-//});
-Route::view("/","registrar_consola")->name("registrar_consola");
-//Route::get("/registrar_juego", function(){
-//    return view("registrar_juego");
-//});
-Route::view("/registrar_juego","registar_juego")->name("registrar_juego");
-//Route::get("/registrar_consola", function(){
-//    return view("registrar_consola");
-//});
-Route::view("/ver_consolas","ver_consolas")->name("ver_consolas");
-//Route::get("/ver_consolas", function(){
-//    return view("ver_consolas");
-//});
 
-//Route::get("/ver_juegos", function(){
-//    return view("ver_juegos");
-//});
+Route::view("/","home")->name("home");
+
+Route::view("/ver_consolas","ver_consolas")->name("ver_consolas");
+
 Route::view("/ver_juegos","ver_juegos")->name("ver_juegos");
+
+Route::view("/agregar_juego","agregar_juego")->name("agregar_juego");
