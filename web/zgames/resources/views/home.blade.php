5f8d6830674bdde75cde761c5aaff8c9e2a71c45
Benjamin <benja13bebv@gmail.com>
2021-06-10 20:23:24 -0400

    Agregar Consola y ver consola

--- web/zgames/resources/views/home.blade.php
+++ web/zgames/resources/views/home.blade.php
@@ -2,4 +2,35 @@
 
 @section("contenido")
 
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-5 mx-auto">
+            <div class="card">
+                <div class="cardheader bg-warning text-white">
+                    <span>Agregar Consola</span>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="nombre-txt" class="form-label">Nombre</label>
+                        <input type="text"  id="nombre-txt" class="form-control" placeholder="Ingrese nombre">
+                    </div>
+                    <div class="mb-3">
+                        <label for="marca-select" class="form-label">Marca</label>
+                        <select  id="marca-select" class="form-select">
+                            <option value="Microsoft">Microsoft</option>
+                            <option value="Sony">Sony</option>
+                            <option value="Nintendo">Nintendo</option>
+                            <option value="Sega">Sega</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="anio-txt" class="form-label">Año de lanzamiento</label>
+                        <input type="number" class="form-control" id="anio-txt">
+                    </div>
+                </div>
+                <div class="card-footer d-grid gap-1">
+                    <button class="btn btn-info">Registrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- web/zgames/resources/views/ver_consolas.blade.php
+++ web/zgames/resources/views/ver_consolas.blade.php
@@ -2,4 +2,25 @@
 
 @section("contenido")
 
+    <div class="row mt-5">
+        <div class="col-12 col-md-12 col-lg-6 mx-auto">
+            <div class="card">
+            <div class="card-body">
+                <table class="table table-bordered table-hover table-striped table-responsive">
+                    <thead class="bg-info">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Marca</th>
+                        <th>Año de Lanzamiento</th>
+                        <th>Acciones</th>
+                    </tr>
+                    </thead>
+                    <tbody id="tbody-consola">
+
+                    </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
+    </div>
 @endsection
